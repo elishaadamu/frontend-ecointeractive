da4a8c88edd16016cdb3f14889a133231b44a7c8
elishaadamu <elishadamu97@gmail.com>
2025-09-12 18:48:06 +0100

    new project

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
+  iconUrl: 'leaflet/dist/images/marker-icon.png',
+  shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+});
 import ProjectPopup from "./ProjectPopup";
 import * as XLSX from "xlsx";
 import { ToastContainer, toast } from "react-toastify";
